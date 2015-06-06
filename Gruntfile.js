e6048741de1288ea3b40db731450a6779d157969
michbuett <michbuett@gmx.de>
2015-06-06 17:04:29 +0200

    Add node-webkit build

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+/* global module */
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        nodewebkit: {
+            options: {
+                appName: 'datadriven-uis',
+                platforms: ['win', 'linux32', 'linux64'],
+                buildDir: 'build',
+            },
+
+            src: [
+                'package.json',
+                'support/alchemy/lib/**/*',
+                'node_modules/alchemy.js/lib/**/*',
+                'src/**/*',
+            ]
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+    grunt.registerTask('build', ['jshint', 'nodewebkit']);
+};
